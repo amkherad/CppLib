8672be81a8c165228fc594e809d39a420f23e33b
Ali Mousavi Kherad <alimousavikherad@gmail.com>
2020-01-31 02:54:31 +0330

    Two basic tests fro TaskScheduler

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -3,15 +3,30 @@
 
 #include <stdio.h>
 #include <threading/TaskScheduler.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace CppLib;
+
+
 
 int main(int argc, char* argv[]) {
 
     TaskScheduler scheduler;
 
-    //scheduler.add()
+    scheduler.add(
+            [](void* stt) {
+                cout << "Task1 was called.";
+                },
+            nullptr
+            );
+
+    scheduler.add(
+            [](void* stt) {
+                cout << "Task2 was called.";
+                },
+            nullptr
+            );
 
     scheduler.captureCurrentThread();
 
