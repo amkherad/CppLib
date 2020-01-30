bd194d8e25e4bed19ef94b9721bab5a6341a0c00
Ali Mousavi Kherad <alimousavikherad@gmail.com>
2020-01-31 02:44:32 +0330

    Fixed CMakeLists bug

---
+++ tests/src/main.cpp
@@ -0,0 +1,18 @@
+//
+//
+
+#include <stdio.h>
+#include <threading/TaskScheduler.hpp>
+
+using namespace std;
+using namespace CppLib;
+
+int main(int argc, char* argv[]) {
+
+    TaskScheduler scheduler;
+
+    //scheduler.add()
+
+    scheduler.captureCurrentThread();
+
+}
